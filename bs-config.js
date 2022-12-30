6e644428d71375217f2603e15c78f8413839a1de
Kazuhito Tsujita <kazuhito.tsujita@gmail.com>
2022-12-30 18:42:53 +0900

    開発環境

---
+++ bs-config.js
@@ -0,0 +1,22 @@
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    "files": [
+      "dist/**/*.html",
+      "dist/js/*.js",
+      "dist/css/*.css",
+    ],
+    "server": "dist",//
+};
---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    require('autoprefixer')({
+      grid: 'autoplace`',
+      flexbox: 'no-2009'
+    }),
+  },
+}
